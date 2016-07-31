05295d744269290a05f43fe375959796b9bf4e50
Ubuntu <ubuntu@ip-172-31-16-238.ap-northeast-2.compute.internal>
2016-07-31 13:05:24 +0000

    poll program fix errors

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url, include
 from django.contrib import admin
 from polls import views
 
-urlpatterns =['',
-    url(r'^polls/$', views.index, name='index'),
-    url(r'^polls/(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^polls/(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^polls/(?P<question_id>\d+)/results/$', views.results, name='results'),
-]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+ ]
--- polls/views.py
+++ polls/views.py
@@ -8,26 +8,38 @@ from polls.models import Choice, Question
 
 
 def index(request):
-	latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_quesion_list': latest_question_list}
-	return render(request, 'poll/index.html', context)
+    print("index in")
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	question == get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def vote(request, question_id):
-	p = get_obejct_or_404(Question, pk = question_id)
-	try:
-		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-	except(KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {'question': p, 'error_message': "not selected.",})
-	else:
-		seleced_choice.votes += 1
-		selected_choice.save()
-		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    print("vote in")
+    p = get_object_or_404(Question, pk=question_id)
+    print("vote 0")
+    
+    try:
+        print("vote 1")
+        print(request.method)
+        print("choice {0}".format(request.POST['choice']))
+        print("vote 1-1")
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+        print("vote 2")
+
+    except(KeyError, Choice.DoesNotExist):
+        print("vote3")
+        return render(request, 'polls/detail.html', {'question': p, 'error_message': "not selected.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        print("selected")
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
